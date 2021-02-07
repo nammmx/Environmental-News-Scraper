101bd23aa0f05e87cfe059f6d8bf7a84e5b37e71
Hai Nam Nguyen <49573677+nammmx@users.noreply.github.com>
2021-02-07 22:31:30 +0100

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
+
+app = Flask(__name__)
+
+ENV = "prod"
+if ENV =="dev":
+  app.debug = True
+  #development database
+  app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:Truongnam97@localhost/news"
+else:
+  app.debug = False
+  #production database
+  app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://wycflgaswbzmjy:e171de134d16cf2d9efb9aa22a4698693199edfe6e2d031bdee0c735fba98354@ec2-3-214-3-162.compute-1.amazonaws.com:5432/d7koeqct7hhh7"
+
+#warning
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+#delete cache
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+
+#database object
+db = SQLAlchemy(app)
+
+
+  #create model
+class News(db.Model):
+  __tablename__ = "news"
+  id = db.Column(db.Integer, primary_key=True)
+  source = db.Column(db.String(200))
+  date = db.Column(db.String(200))
+  headline = db.Column(db.String(200))
+  link = db.Column(db.String(200))
+
+  #initializer/constructor
+  def __init__(self, source, date, headline, link):
+    self.source = source
+    self.date = date
+    self.headline = headline
+    self.link = link
+
+now = datetime.now()
+
+########################################### GUARDIAN ##################################################
+source_guardian = requests.get("https://www.theguardian.com/environment/all").text
+soup_guardian = BeautifulSoup(source_guardian, "lxml")
+
+#todays date - guardian
+date_guardian = now.strftime("%#d-%B-%Y").lower()
+
+print("---------- GUARDIAN ----------")
+section_guardian = soup_guardian.find("section", {"id": date_guardian})
+#header_guardian = section_guardian.a.text
+#print(header_guardian)
+try:
+  for section_row in section_guardian.find_all("div", class_="fc-slice-wrapper"):
+    for links in section_row.find_all("div", class_="fc-item__container"):
+      for links_link in links.find_all("a", class_="u-faux-block-link__overlay js-headline-text"):
+        title_guardian = links_link.text
+        href_guardian = links_link.get("href")
+        #insert in database
+        #if not db.session.query(News).filter(News.link == href_guardian).count():
+        new_articles_guardian = News("guardian", now.strftime("%d %b %Y"), title_guardian, href_guardian)
+        db.session.add(new_articles_guardian)
+        db.session.commit()    
+except Exception as e:
+  pass
+
+
+@app.route('/')
+def home():
+  days = []
+  news_dates = db.session.query(News).distinct(News.date)
+  for dates in news_dates:
+    days.append(dates.date)
+  news_now = {}
+  for day in days:
+    news_now[day] = db.session.query(News).filter(News.date == day).all()
+  #news_now = db.session.query(News).filter(News.date == day).all()
+  return render_template("index.html", news_now=news_now, days=days)
+
+#run app
+if __name__ == "__main__":
+  app.run()
